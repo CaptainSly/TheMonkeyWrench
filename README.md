0ea393a3824699cda1f0208e16f185ddb481da80
Zachary Knoblauch <CaptainSly@users.noreply.github.com>
2021-12-01 16:53:13 -0600

    The Initial Commit
    Signed-off-by: Zachary Knoblauch <CaptainSly@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+![The Monkey Wrench](src/main/resources/logo.png)
+=================================================
+
+The Monkey Wrench is A Tech mod, for 1.17 and up that allows you to automate machines, process resources, craft new
+tools, items and blocks. Eat new foods and manage a new type of storage system.
---
+++ src/main/java/captainsly/monkeywrench/MonkeyWrenchMod.java
@@ -0,0 +1,50 @@
+/*
+ *    Copyright [2021] [Azraein]
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package captainsly.monkeywrench;
+
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Locale;
+
+@Mod(MonkeyWrenchMod.MOD_ID)
+public class MonkeyWrenchMod {
+
+    public static final String MOD_ID = "monkeywrench";
+
+    private static final Logger LOGGER = LogManager.getLogger(MonkeyWrenchMod.MOD_ID.toUpperCase(Locale.US));
+
+    public MonkeyWrenchMod() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+}
